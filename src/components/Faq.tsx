08239111b03f1d05a8cf5f8acc87e493043509ed
Magdalena Senger <magdal.sen@gmail.com>
2023-05-15 12:17:51 +0200

    add basic faq

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,7 +1,40 @@
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box } from "@chakra-ui/react";
+
+const styles = { bg: 'blueviolet', color: 'white' };
 
 const Faq = () => (
         <>
-            FAQ
+        <Accordion defaultIndex={[0]} allowMultiple width='500px'>
+            <AccordionItem>
+                <h2>
+                <AccordionButton _expanded={styles}>
+                    <Box as="span" flex='1' textAlign='left'>
+                    Na czym polega GoToShop?
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                Polega na wzajemnej - zazwyczaj sąsiedzkiej - pomocy w robieniu zakupów. Możesz poszukać adresu, który jest w Twojej
+                okolicy i przy okazji robienia swoich zakupów - zrealizować listę zakupów sąsiada lub znajomego.
+                </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+                <h2>
+                <AccordionButton _expanded={styles}>
+                    <Box as="span" flex='1' textAlign='left'>
+                    Jaka jest idea?
+                    </Box>
+                    <AccordionIcon />
+                </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                Jest kilka myśli przewodnich: oszczędność czasu, otrzymanie napiwku w wielkości zdefiniowanej przez odbiorcę zakupów,
+                poznanie sąsiadów lub osób mieszkających w Twojej okolicy.
+                </AccordionPanel>
+            </AccordionItem>
+            </Accordion>
         </>
     )
 
